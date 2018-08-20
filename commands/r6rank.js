69125550a1df2f1a99aed15fbcf5d51f42a5fea9
zvshka <39166475+zvshka@users.noreply.github.com>
2018-08-20 19:49:59 +0300

    2

---
+++ commands/r6rank.js
@@ -0,0 +1,26 @@
+const R6Api = require('r6-discord');
+const R6 = new R6Api();
+
+const username = args[0];
+const platform = args[1];
+
+
+module.exports.run = async (bot, message, args) => { // Get stats on a user on that platform.
+    
+    R6.stats(username, platform, /* Optional Boolean if you want operator stats. */ ).then(response => {
+        console.log(response);
+    }).catch(error => {
+        console.error(error);
+    });
+
+    // Get details on a user on R6 depending on platform.
+    R6.profile(username, platform).then(response => {
+        console.log(response);
+    }).catch(error => {
+        console.error(error);
+    });
+}
+
+module.exports.help = {
+    name: "r6rank"
+}
