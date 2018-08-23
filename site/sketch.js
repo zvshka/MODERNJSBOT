e014f2f3dfd3ab60faa56b44eab59e6c4c8af729
zvshka <39166475+zvshka@users.noreply.github.com>
2018-08-23 14:15:35 +0300

    site eee

---
+++ site/sketch.js
@@ -0,0 +1,25 @@
+var canvas
+function setup() {
+  //var gui = createGui("Discord")
+  //gui.addGlobals();
+  tecst = createP("Craft is Life")
+  tecst.position(40, -25);
+  tecst.style("font-family", "monospace");
+  tecst.style("color", "#FFFFFF");
+  tecst.style("font-size", "30pt");
+  tecst.style("padding", "10px");
+  head()
+}
+
+function head() {
+  canvas = createCanvas(innerWidth, 100)
+}
+
+function head2() {
+  canvas = createCanvas(innerWidth, 100)
+  
+}
+
+function draw() {
+  background(50, 50, 50)
+}
