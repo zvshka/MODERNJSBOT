a6263b8b0e1e64dbdd119ef8d22b0b7e4eb54b48
zvshka <39166475+zvshka@users.noreply.github.com>
2018-09-13 15:03:16 +0300

    wou

--- commands/coins.js
+++ commands/coins.js
@@ -21,7 +21,7 @@ module.exports.run = (bot, message, args) => {
   let coinEmbed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor("#00FF00")
-  .addField(":dollar:", uCoins);
+  .addField(":dollar:", `**${uCoins}**`);
 
   message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
 
