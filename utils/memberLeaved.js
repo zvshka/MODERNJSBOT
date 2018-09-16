40090ec5dabbde6a32c65b4fd8e24bbeb02afb8a
zvshka <39166475+zvshka@users.noreply.github.com>
2018-09-16 16:54:20 +0300

    fck

---
+++ utils/memberLeaved.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  process: (bot, GuildMember) => {
+    const User = GuildMember.user;
+    const userLogsChannel = GuildMember.guild.channels.find(c => c.name === 'welcome');
+
+    userLogsChannel.send(
+      ':white_check_mark: ' +
+      User.toString() + ' ' +
+      '(' + User.username + ') ' +
+      '`' + User.id + '` Покинул нас ' +
+      new Date().toUTCString()
+    );
+    
+  }
+};  
