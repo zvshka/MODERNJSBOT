b3623caca7ee69244a6bf4fac0af362f486e559a
zvshka <39166475+zvshka@users.noreply.github.com>
2018-09-21 14:46:13 +0300

    ухх

--- utils/memberBanned.js
+++ utils/memberBanned.js
@@ -6,28 +6,27 @@ module.exports = {
         if (!logs) return;
         let date = Date.now()
         const entry = GuildMember.guild.fetchAuditLogs({
-            type: 'MEMBER_BAN_ADD'
-        })
-        .then(audit => audit.entries.first())
-        .then(entry => {
-            if(!entry.executor.bot) {
-                let date_m = date - entry.createdTimestamp
-                console.log(date_m)
-                if (date_m < 3000) {
-                let banEmbed = new Discord.RichEmbed()
-                .setDescription("~Ban~")
-                .setColor("#bc0000")
-                .addField("Забанен", `<@${entry.target.id}> с ID ${entry.target.id}`)
-                .addField("Забанил", `<@${entry.executor.id}> с ID ${entry.executor.id}`)
-                .addField("Забанен в", "NaN")
-                .addField("Время", entry.createdAt)
-                .addField("Причина", entry.reason);
-                logs.send(banEmbed)
+                type: 'MEMBER_BAN_ADD'
+            })
+            .then(audit => audit.entries.first())
+            .then(entry => {
+                if (!entry.executor.bot) {
+                    let date_m = date - entry.createdTimestamp
+                    console.log(date_m)
+                    if (date_m < 3000) {
+                        let banEmbed = new Discord.RichEmbed()
+                            .setDescription("~Ban~")
+                            .setColor("#bc0000")
+                            .addField("Забанен", `<@${entry.target.id}> с ID ${entry.target.id}`)
+                            .addField("Забанил", `<@${entry.executor.id}> с ID ${entry.executor.id}`)
+                            .addField("Забанен в", "NaN")
+                            .addField("Время", entry.createdAt)
+                            .addField("Причина", entry.reason);
+                        logs.send(banEmbed)
+                    }
                 }
-                console.log(entry, date)
-            }
-        })
-        
+            })
+
     }
 
 };
--- utils/memberKicked.js
+++ utils/memberKicked.js
@@ -6,28 +6,27 @@ module.exports = {
         if (!logs) return;
         let date = Date.now()
         const entry = GuildMember.guild.fetchAuditLogs({
-            type: 'MEMBER_KICK'
-        })
-        .then(audit => audit.entries.first())
-        .then(entry => {
-            if(!entry.executor.bot) {
-                let date_m = date - entry.createdTimestamp
-                console.log(date_m)
-                if (date_m < 3000) {
-                let kickEmbed = new Discord.RichEmbed()
-                .setDescription("~Kick~")
-                .setColor("#e56b00")
-                .addField("Кикнут", `<@${entry.target.id}> с ID ${entry.target.id}`)
-                .addField("Кикнул", `<@${entry.executor.id}> с ID ${entry.executor.id}`)
-                .addField("Кикнут в", "NaN")
-                .addField("Время", `${entry.createdAt}`)
-                .addField("Причина", `${entry.reason}`)
-                logs.send(kickEmbed)
+                type: 'MEMBER_KICK'
+            })
+            .then(audit => audit.entries.first())
+            .then(entry => {
+                if (!entry.executor.bot) {
+                    let date_m = date - entry.createdTimestamp
+                    console.log(date_m)
+                    if (date_m < 3000) {
+                        let kickEmbed = new Discord.RichEmbed()
+                            .setDescription("~Kick~")
+                            .setColor("#e56b00")
+                            .addField("Кикнут", `<@${entry.target.id}> с ID ${entry.target.id}`)
+                            .addField("Кикнул", `<@${entry.executor.id}> с ID ${entry.executor.id}`)
+                            .addField("Кикнут в", "NaN")
+                            .addField("Время", `${entry.createdAt}`)
+                            .addField("Причина", `${entry.reason}`)
+                        logs.send(kickEmbed)
+                    }
                 }
-            }
-            console.log(entry)
-        })
-        
+            })
+
     }
 
 };
