6e10b9fc0ac22f4afbc89678ca988bdf305b241c
zvshka <39166475+zvshka@users.noreply.github.com>
2018-10-20 10:52:46 +0300

    ok

--- bot.js
+++ bot.js
@@ -163,7 +163,7 @@ bot.on("message", message => {
 
 
   let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
+  let cmd = messageArray[0].toLocaleLowerCase();
   let args = messageArray.slice(1);
 
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
