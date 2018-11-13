16c01e09e64f4481e9d7ad6aa43eafa34a1c9515
zvshka <39166475+zvshka@users.noreply.github.com>
2018-11-13 13:46:01 +0300

    ЩЛ

--- bot.js
+++ bot.js
@@ -67,108 +67,108 @@ bot.on("ready", () => {
 
 //Исполнение команд
 bot.on("message", (msg) => {
-  if(msg) {//Проверка на то что создатель сообщения бот и канал DM
-  if (msg.author.bot) return
-  if (msg.channel.type === "dm") return;
-  //Монетки :D
+  if (msg) { //Проверка на то что создатель сообщения бот и канал DM
+    if (msg.author.bot) return
+    if (msg.channel.type === "dm") return;
+    //Монетки :D
 
-  msg.guild.fetchMember(msg.author).then(m => console.log(`[log]${msg.guild.name}: ${m.displayName}: ${msg}`))
+    msg.guild.fetchMember(msg.author).then(m => console.log(`[log]${msg.guild.name}: ${m.displayName}: ${msg}`))
 
-  function cmdrun(prefix) {
-    if (!msg.content.startsWith(prefix)) return;
-    if (cooldown.has(msg.author.id)) {
-      msg.delete();
-      return msg.reply("Подожди 5 секунд.")
-    }
-    if (!msg.member.hasPermission("ADMINISTRATOR")) {
-      cooldown.add(msg.author.id);
-    }
-    let args = msg.content.slice(prefix.length).trim().split(' ')
-    console.log(args)
-    let cmd = args.shift().toLowerCase();
-    try {
-      let commandfile = require(`./commands/${cmd}`)
-      if (commandfile) {
-        commandfile.run(bot, msg, args);
-      } else {
-        let coinAmt = Math.floor(Math.random() * 10) + 100;
-        let baseAmt = Math.floor(Math.random() * 10) + 100;
-        if (coinAmt === baseAmt) {
-          let userdb = db.connect(process.env.USERSDB, {
-            useNewUrlParser: true
-          }).then(res => {
-            Coins.findOne({
-              UserID: msg.author.id,
-              ServerID: msg.guild.id,
-            }, (err, money) => {
-              if (err) {
-                console.log(err)
-              }
-              if (!money) {
-                const newMoney = new Coins({
-                  UserID: msg.author.id,
-                  ServerID: msg.guild.id,
-                  money: coinAmt,
-                })
-                newMoney.save().catch(err => console.log(err.stack))
-              } else {
-                money.money += coinAmt
-                money.save().catch(err => console.log(err.stack))
-              }
+    function cmdrun(prefix) {
+      if (!msg.content.startsWith(prefix)) return;
+      if (cooldown.has(msg.author.id)) {
+        msg.delete();
+        return msg.reply("Подожди 5 секунд.")
+      }
+      if (!msg.member.hasPermission("ADMINISTRATOR")) {
+        cooldown.add(msg.author.id);
+      }
+      let args = msg.content.slice(prefix.length).trim().split(' ')
+      console.log(args)
+      let cmd = args.shift().toLowerCase();
+      try {
+        let commandfile = require(`./commands/${cmd}`)
+        if (commandfile) {
+          commandfile.run(bot, msg, args);
+        } else {
+          let coinAmt = Math.floor(Math.random() * 10) + 100;
+          let baseAmt = Math.floor(Math.random() * 10) + 100;
+          if (coinAmt === baseAmt) {
+            let userdb = db.connect(process.env.USERSDB, {
+              useNewUrlParser: true
+            }).then(res => {
+              Coins.findOne({
+                UserID: msg.author.id,
+                ServerID: msg.guild.id,
+              }, (err, money) => {
+                if (err) {
+                  console.log(err)
+                }
+                if (!money) {
+                  const newMoney = new Coins({
+                    UserID: msg.author.id,
+                    ServerID: msg.guild.id,
+                    money: coinAmt,
+                  })
+                  newMoney.save().catch(err => console.log(err.stack))
+                } else {
+                  money.money += coinAmt
+                  money.save().catch(err => console.log(err.stack))
+                }
+              })
             })
-          })
 
-          let coinEmbed = new Discord.RichEmbed()
-            .setAuthor(msg.author.username)
-            .setColor("#3de2fa")
-            .addField("💸", `${coinAmt} монет добавлено!`);
+            let coinEmbed = new Discord.RichEmbed()
+              .setAuthor(msg.author.username)
+              .setColor("#3de2fa")
+              .addField("💸", `${coinAmt} монет добавлено!`);
 
-          msg.channel.send(coinEmbed).then(msg => {
-            msg.delete(5000)
-          });
+            msg.channel.send(coinEmbed).then(msg => {
+              msg.delete(5000)
+            });
+          }
         }
+
+        setTimeout(() => {
+          cooldown.delete(msg.author.id)
+        }, cdseconds * 1000)
+
+      } catch (err) {
+        console.log(err)
       }
-
-      setTimeout(() => {
-        cooldown.delete(msg.author.id)
-      }, cdseconds * 1000)
-
-    } catch (err) {
-      console.log(err)
     }
-  }
-  //Опять префикс
-  let serverdb = db.connect(process.env.SERVERSDB, {
-    useNewUrlParser: true
-  }).then(res => {
-    Options.findOne({
-      ServerID: msg.guild.id
-    }, (err, opts) => {
-      if (err) {
-        console.log(err.stack)
-      }
-      if (!opts) {
-        const newOpts = new Options({
-          ServerID: msg.guild.id,
-          Prefix: botconfig.prefix,
-          AutoRole: "off"
-        })
-        newOpts.save().catch(err => console.log(err.stack))
-        cmdrun(newOpts.Prefix)
-      } else {
-        try {
-          cmdrun(opts.Prefix)
-        } catch (e) {
-          console.log(err)
+    //Опять префикс
+    let serverdb = db.connect(process.env.SERVERSDB, {
+      useNewUrlParser: true
+    }).then(res => {
+      Options.findOne({
+        ServerID: msg.guild.id
+      }, (err, opts) => {
+        if (err) {
+          console.log(err.stack)
         }
-      }
+        if (!opts) {
+          const newOpts = new Options({
+            ServerID: msg.guild.id,
+            Prefix: botconfig.prefix,
+            AutoRole: "off"
+          })
+          newOpts.save().catch(err => console.log(err.stack))
+          cmdrun(newOpts.Prefix)
+        } else {
+          try {
+            cmdrun(opts.Prefix)
+          } catch (e) {
+            console.log(err)
+          }
+        }
+      })
     })
-  })
   }
 
-if(msg) {
-  console.log(msg, msg.content, msg.content.split(" "))
-}
+  if (msg) {
+    console.log(msg, msg.content, msg.content.split(" "))
+  }
 });
 //Выдача роли когда кто-то вступает и приветствие
 bot.on('guildMemberAdd', (member) => {
