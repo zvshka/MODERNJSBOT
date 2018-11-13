9ce483bfde3cdb6ca31495b373bbd7ba165defdd
zvshka <39166475+zvshka@users.noreply.github.com>
2018-11-13 10:00:58 +0300

    mongodb sys

--- commands/coins.js
+++ commands/coins.js
@@ -1,31 +1,48 @@
 const Discord = require("discord.js");
 const fs = require('fs');
-
+const db = require('mongoose')
+const Coins = require("../models/coins.js")
+db.connect(process.env.USERSDB, {
+  useNewUrlParser: true
+})
 module.exports.run = (bot, message, args) => {
   //!coins
-  let coins = JSON.parse(fs.readFileSync("./utils/coins.json", "utf8"))
-  
-  if(!coins[message.guild.id]) {
-    coins[message.guild.id] = {};
+    Coins.findOne({
+      UserID: message.author.id,
+      ServerID: message.guild.id,
+    }, (err, coins) => {
+      if (err) {
+        console.log(err)
+      }
+      if (!money) {
+        const newMoney = new Coins({
+          UserID: message.author.id,
+          ServerID: message.guild.id,
+          money: 0,
+        })
+        newMoney.save().catch(err => console.log(err.stack))
+        let coinEmbed = new Discord.RichEmbed()
+          .setAuthor(message.author.username)
+          .setColor("#00FF00")
+          .addField(":dollar:", `**${newMoney.money}**`);
+
+        message.channel.send(coinEmbed).then(msg => {
+          msg.delete(5000)
+        });
+
+      } else {
+        let coinEmbed = new Discord.RichEmbed()
+          .setAuthor(message.author.username)
+          .setColor("#00FF00")
+          .addField(":dollar:", `**${coins.money}**`);
+
+        message.channel.send(coinEmbed).then(msg => {
+          msg.delete(5000)
+        });
+      
+      }
+    })
   }
-
-  if(!coins[message.guild.id][message.author.id]){
-    coins[message.guild.id][message.author.id] = {
-      coins: 0
-    };
-  }
-
-  let uCoins = coins[message.guild.id][message.author.id].coins;
-
-
-  let coinEmbed = new Discord.RichEmbed()
-  .setAuthor(message.author.username)
-  .setColor("#00FF00")
-  .addField(":dollar:", `**${uCoins}**`);
-
-  message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
-
-}
 
 module.exports.help = {
   name: "coins",
